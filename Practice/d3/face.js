6925db3faf9e00e30579744e0d0a5cc1a90baf76
maxime <maxime.van.uffelen@vub.be>
2021-12-16 16:16:17 +0100

    completed first basic tutorial

--- Practice/d3/face.js
+++ Practice/d3/face.js
@@ -3,18 +3,39 @@
     source: https://www.youtube.com/watch?v=2LhoCfjm8R4&list=PLee96XBF3UtuOHagD-SxtXP3QI2ERrc34 
 */
 
+const width = 960;
+const height = 500;
+const centerX = width / 2;
+const centerY = height / 2;
+const strokeWidth = 10;
+const eyeOffsetX = 90;
+const eyeOffsetY = 100;
+const eyeRadius = 50;
+const mouthWidth = 20;
+const mouthRadius = 140;
+
+const mouthArc = d3
+  .arc()
+  .innerRadius(mouthRadius)
+  .outerRadius(mouthRadius + mouthWidth)
+  .startAngle(Math.PI / 2)
+  .endAngle((Math.PI * 3) / 2);
+
 const App = () => {
   return (
     <div className="app-content">
-      <svg width="960" height="500">
-        <circle
-          cx="480"
-          cy="250"
-          r="245"
-          fill="yellow"
-          stroke="black"
-          strokeWidth="10"
-        />
+      <svg width={width} height={height}>
+        <g transform={`translate(${centerX},${centerY})`}>
+          <circle
+            r={centerY - strokeWidth / 2}
+            fill="yellow"
+            stroke="black"
+            strokeWidth={strokeWidth}
+          />
+          <circle cx={-eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius} />
+          <circle cx={eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius} />
+          <path d={mouthArc()} />
+        </g>
       </svg>
     </div>
   );
