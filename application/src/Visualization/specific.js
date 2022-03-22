3791b1df95711a6b393b81925667a3bd82ae675a
maxime <maxime.van.uffelen@vub.be>
2022-03-22 23:33:03 +0100

    more specific/detailed view of one state the user selected

---
+++ application/src/Visualization/specific.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// Specific visualization in more detail of one state the user clicked on
+
+const Specific = () => {
+  return <div className="visualization-element">Specific</div>;
+};
+
+export default Specific;
