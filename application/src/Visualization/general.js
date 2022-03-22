663e9d4c6e1aecefa3e960bb7c8b701a73ae2a00
maxime <maxime.van.uffelen@vub.be>
2022-03-22 23:32:38 +0100

    the general view of the states

---
+++ application/src/Visualization/general.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// General visualisation of the states of the CRDTs
+
+const General = () => {
+  return (
+    <div className="visualization-element">
+      <h2>Visualization</h2>
+      General
+    </div>
+  );
+};
+
+export default General;
