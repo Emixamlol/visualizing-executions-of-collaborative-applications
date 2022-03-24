2aea8856438dee55e3f11ea1165918465312e691
maxime <maxime.van.uffelen@vub.be>
2022-03-24 22:41:17 +0100

    added js methods apply() and call() examples

--- application/src/Proxy/index.js
+++ application/src/Proxy/index.js
@@ -5,8 +5,31 @@ import { reducer } from './reducer';
 const proxies = [];
 const defaultState = {};
 
-const Index = ({ crdt }) => {
+const Index = ({ id, crdt }) => {
   const [state, dispatch] = useReducer(reducer, defaultState);
+
+  /* https://www.w3schools.com/js/js_function_call.asp */
+
+  const person = {
+    fullName: function () {
+      return this.firstName + ' ' + this.lastName;
+    },
+  };
+  const person1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+  };
+  const person2 = {
+    firstName: 'Mary',
+    lastName: 'Doe',
+  };
+  const f = (fname, objname) => person[fname].call(objname);
+  console.log(f('fullName', person2));
+
+  const g = (fname, objname) => person[fname].apply(objname);
+  console.log(g('fullName', person1));
+
+  /* https://www.w3schools.com/js/js_function_apply.asp */
 
   const query = () => {};
 
@@ -16,12 +39,7 @@ const Index = ({ crdt }) => {
 
   const apply = () => {};
 
-  return (
-    <>
-      <Framework />
-      <div>Index</div>
-    </>
-  );
+  return null; // a Proxy itself does not render anything
 };
 
 export default Index;
