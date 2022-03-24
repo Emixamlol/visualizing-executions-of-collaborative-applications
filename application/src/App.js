ad7d13fe6a36ffc14be8efcb832fa87c90038684
maxime <maxime.van.uffelen@vub.be>
2022-03-24 22:39:54 +0100

    implemented proxy functionality

--- application/src/App.js
+++ application/src/App.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Proxy from './Proxy';
+import { ProxyContext } from './Proxy/proxy-context';
 import Visualization from './Visualization';
 import REPL from './REPL';
 
 const App = () => {
+  // create a Proxy context so that the REPL and Visualization components, together with all their child components, all have access to the proxies
+
+  const [proxies, setProxies] = useState([]);
+
+  const removeProxy = (id) => {
+    setProxies((proxies) => {
+      return proxies.filter((proxy) => proxy.id !== id);
+    });
+  };
+
+  const addProxy = (proxy) => {
+    setProxies((proxies) => {
+      return [...proxies, proxy];
+    });
+  };
+
+  console.log(proxies);
+
+  useEffect(() => {
+    console.log('App useEffect called');
+  });
+
+  // Wrap the application in the ProxyContext.Provider so that any component can have access to the properties from 'value' by using the useContext hook
   return (
-    <div className="grid-container">
-      <Visualization />
-      <REPL />
-    </div>
+    <ProxyContext.Provider value={{ removeProxy, addProxy }}>
+      <div className="grid-container">
+        <Visualization />
+        <REPL />
+        {proxies.map((proxy) => {
+          return <Proxy key={proxy.id} {...proxy} />;
+        })}
+      </div>
+    </ProxyContext.Provider>
   );
 };
 
