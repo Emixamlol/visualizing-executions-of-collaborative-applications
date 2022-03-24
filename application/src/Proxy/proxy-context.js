cd9cf9b4f810b7a3c2ed6679e1646a482f9c3612
maxime <maxime.van.uffelen@vub.be>
2022-03-24 22:41:48 +0100

    created a ProxyContext using the useContext React hook

---
+++ application/src/Proxy/proxy-context.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+// use the useContext hook to create a Proxy Context so that any component can easily have access to the proxies
+export const ProxyContext = React.createContext();
