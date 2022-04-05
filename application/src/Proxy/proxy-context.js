3ba44b3e1577ba74add458ed7ae7be6acb0def23
maxime <maxime.van.uffelen@vub.be>
2022-04-05 15:39:08 +0200

    ProxyContext is now defined and exported elsewhere

--- application/src/Proxy/proxy-context.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-
-// use the useContext hook to create a Proxy Context so that any component can easily have access to the proxies
-export const ProxyContext = React.createContext();
