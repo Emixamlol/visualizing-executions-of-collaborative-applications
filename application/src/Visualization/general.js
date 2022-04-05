cd933da28f41d83157afef7a09894ac9527f56d6
maxime <maxime.van.uffelen@vub.be>
2022-04-05 19:43:53 +0200

    render visualization for all proxies here

--- application/src/Visualization/general.js
+++ application/src/Visualization/general.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ProxyContext } from '../Proxy/state-handling';
+import Proxy from '../Proxy';
 import Demo from '../CRDTs/LWW-Register/demo';
 
 // General visualisation of the states of the CRDTs
 
 const General = () => {
+  const { proxies } = useContext(ProxyContext);
+
   return (
     <div className="visualization-element">
       <h2>Visualization</h2>
-      {/* General */}
-      <Demo />
+      {proxies.map((proxy) => {
+        return <Proxy key={proxy.id} {...proxy} />;
+      })}
+      {/* <Demo /> */}
     </div>
   );
 };
