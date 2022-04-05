01f9f1ad8a5a73064f54a7ab6bd848726e295251
maxime <maxime.van.uffelen@vub.be>
2022-04-05 19:47:28 +0200

    export all implemented crdts

---
+++ application/src/CRDTs/index.js
@@ -0,0 +1,7 @@
+// import this file to import every CRDT
+
+import TPSet from './2P-Set/set';
+import Register from './LWW-Register/register';
+import Counter from './PN-Counter/counter';
+
+export { TPSet, Register, Counter };
