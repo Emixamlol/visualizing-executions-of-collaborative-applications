1ea4c96211bc7392f137ff53fd7f02900670f0b7
maxime <maxime.van.uffelen@vub.be>
2022-04-07 19:14:45 +0200

    data is now called state; this component now receives a proxy as argument

--- application/src/D3-framework/index.js
+++ application/src/D3-framework/index.js
@@ -1,12 +1,38 @@
 import React, { useEffect } from 'react';
 
-const data = [];
-
 const Index = ({ proxy }) => {
-  // useEffect hook, the function defined within this hook is called each time the component is rendered again
-  useEffect(() => {}, [data]); // Render the component if data changes
+  console.log(proxy);
 
-  return <div>{data}</div>;
+  const state = proxy.getState();
+  console.log(`state = ${state}`);
+
+  /* https://www.w3schools.com/js/js_function_call.asp */
+
+  const person = {
+    fullName: function () {
+      return this.firstName + ' ' + this.lastName;
+    },
+  };
+  const person1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+  };
+  const person2 = {
+    firstName: 'Mary',
+    lastName: 'Doe',
+  };
+  const f = (fname, objname) => person[fname].call(objname);
+  console.log(f('fullName', person2));
+
+  const g = (fname, objname) => person[fname].apply(objname);
+  console.log(g('fullName', person1));
+
+  /* https://www.w3schools.com/js/js_function_apply.asp */
+
+  // useEffect hook, the function defined within this hook is called each time the component is rendered again
+  useEffect(() => {}, [state]); // Render the component if state changes
+
+  return <div>{state}</div>;
 };
 
 export default Index;
