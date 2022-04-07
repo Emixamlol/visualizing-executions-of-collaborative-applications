cb5377d0e15c59a82bece3f08124a62699cb2261
maxime <maxime.van.uffelen@vub.be>
2022-04-07 19:12:58 +0200

    removed crdt handler

--- application/src/App.js
+++ application/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ProxyStateHandling from './Proxy/state-handling';
 import { Counter, Register, TPSet } from './CRDTs';
-import { crdt_handler } from './Proxy/crdt-handler';
 import Visualization from './Visualization';
 import REPL from './REPL';
 
