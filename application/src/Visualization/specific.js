d061b377a978ad10b7bbfd5439bc95960572a61f
maxime <maxime.van.uffelen@vub.be>
2022-04-11 20:18:59 +0200

    added svg element to specific visualization component

--- application/src/Visualization/specific.js
+++ application/src/Visualization/specific.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 // Specific visualization in more detail of one state the user clicked on
 
-const Specific = () => {
-  return <div className="visualization-element">Specific</div>;
+const Specific = ({ dimensions, proxy }) => {
+  const {
+    width,
+    height,
+    margin: { top, right, bottom, left },
+  } = dimensions;
+  const svgRef = useRef(null);
+  return (
+    <div className="visualization-element">
+      <svg
+        id="specific-visualization"
+        svgRef={svgRef}
+        width={width}
+        height={height}
+      />
+    </div>
+  );
 };
 
 export default Specific;
