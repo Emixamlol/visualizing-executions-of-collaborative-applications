5b85fe8c6733ffc64ed9a843e0e3367a81cec839
maxime <maxime.van.uffelen@vub.be>
2022-04-11 20:19:40 +0200

    added dimensions; specific component is now conditionally rendered

--- application/src/Visualization/index.js
+++ application/src/Visualization/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { ProxyContext } from '../Proxy/state-handling';
 import General from './general';
 import Specific from './specific';
 
 // This component is responsible for the visualization of the states of the CRDTs
 
 const Index = () => {
+  // define dimensions for the visualization frame
+  const dimensions = {
+    width: '50vw',
+    height: '70vh',
+    margin: { top: 20, right: 20, bottom: 20, left: 20 },
+  };
+
+  const { proxies } = useContext(ProxyContext);
+
+  const [show, setShow] = useState(false); // proxy we show in the specific visualization
+
   return (
     <div id="visualization" className="flexbox-container">
-      <General />
-      <Specific />
+      <General dimensions={dimensions} proxies={proxies} />
+      {show && <Specific dimensions={dimensions} proxy={show} />}
     </div>
   );
 };
