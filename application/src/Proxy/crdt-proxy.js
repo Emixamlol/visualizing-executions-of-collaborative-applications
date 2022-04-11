143a7a2c20dd309bd32aeac0ad88b9363b68db0b
maxime <maxime.van.uffelen@vub.be>
2022-04-11 20:22:43 +0200

    fixed bug (in apply method) ; state is now an object instead of an array

--- application/src/Proxy/crdt-proxy.js
+++ application/src/Proxy/crdt-proxy.js
@@ -7,7 +7,7 @@ export default class CrdtProxy {
   constructor(id, crdt, params) {
     this.id = id;
     this.#crdt = crdt !== undefined ? createCRDT(crdt, params) : crdt;
-    this.#state = [];
+    this.#state = {};
 
     this.query = (...args) => {
       switch (crdt) {
@@ -26,25 +26,26 @@ export default class CrdtProxy {
     this.compare = (other) => this.#crdt.compare(other.#crdt);
 
     this.merge = (other) => {
-      const crdt = this.#crdt.merge(other.#crdt);
       const proxy = new CrdtProxy(this.id);
-      proxy.#crdt = crdt;
+      proxy.#crdt = this.#crdt.merge(other.#crdt);
       return proxy;
     };
 
     this.apply = (fn, params) => {
       console.log('applying in crdtProxy');
-      this.#crdt[fn].apply(params); // apply the function
-      return this.#updateState(); // update the state and return it
+      console.log(fn, params);
+      this.#crdt[fn].apply(this.#crdt, params); // apply the function on the crdt
+      return this.#updateState(crdt); // update the state and return it
     };
   }
 
   // this method updates the state and returns a copy of it after the update
   #updateState = (crdt) => {
     switch (crdt) {
-      case 'counter':
-        this.#state.push();
+      case 'counter': {
+        // this.#state.push();
         break;
+      }
 
       case 'register':
         break;
@@ -58,5 +59,7 @@ export default class CrdtProxy {
     return this.getState();
   };
 
-  getState = () => this.#state.slice(); // can only get copy of the data so that it cannot be changed directly
+  getState = () => {
+    return { ...this.#state }; // can only get copy of the data so that it cannot be changed directly
+  };
 }
