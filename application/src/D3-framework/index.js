055063d8a558dfafd03d408c236ce884bafaec7d
maxime <maxime.van.uffelen@vub.be>
2022-04-11 20:23:51 +0200

    started implementing d3 functionality;  visualizing components are now dependant on proxies

--- application/src/D3-framework/index.js
+++ application/src/D3-framework/index.js
@@ -1,38 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import * as d3 from 'd3';
 
-const Index = ({ proxy }) => {
-  console.log(proxy);
+const Index = ({ proxies, dimensions, svgRef }) => {
+  const {
+    width,
+    height,
+    margin: { top, right, bottom, left },
+  } = dimensions;
 
-  const state = proxy.getState();
-  console.log(`state = ${state}`);
+  // const [state, setState] = useState(proxy.getState());
+  // console.log(state);
 
-  /* https://www.w3schools.com/js/js_function_call.asp */
-
-  const person = {
-    fullName: function () {
-      return this.firstName + ' ' + this.lastName;
-    },
-  };
-  const person1 = {
-    firstName: 'John',
-    lastName: 'Doe',
-  };
-  const person2 = {
-    firstName: 'Mary',
-    lastName: 'Doe',
-  };
-  const f = (fname, objname) => person[fname].call(objname);
-  console.log(f('fullName', person2));
-
-  const g = (fname, objname) => person[fname].apply(objname);
-  console.log(g('fullName', person1));
-
-  /* https://www.w3schools.com/js/js_function_apply.asp */
+  console.log(proxies);
+  const svg = d3.select(svgRef.current);
+  const idDiv = svg.append('g');
 
   // useEffect hook, the function defined within this hook is called each time the component is rendered again
-  useEffect(() => {}, [state]); // Render the component if state changes
+  useEffect(() => {
+    // const svg = d3.select(svgRef.current);
+    // const idDiv = svg.append('g');
 
-  return <div>{state}</div>;
+    idDiv
+      .selectAll('text')
+      .data(Array.from(proxies))
+      .enter()
+      .append('text')
+      .attr('x', (d, i) => i * 20)
+      .attr('y', 70)
+      .attr('fill', 'black')
+      .text(([id, proxy]) => id);
+
+    // console.log(proxy);
+    console.log(svg);
+
+    console.log(idDiv);
+
+    return () => {
+      // idDiv.remove();
+      idDiv.selectAll('text').data(Array.from(proxies)).exit().remove();
+    };
+  }, [proxies]); // Render the component if state changes
+
+  return null;
 };
 
 export default Index;
