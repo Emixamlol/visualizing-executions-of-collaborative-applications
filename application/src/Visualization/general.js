5c0f510317355a0e936c0032677b4f098fc64d2f
maxime <maxime.van.uffelen@vub.be>
2022-04-11 20:21:13 +0200

    added dimensions; framework component now gets passed all proxies

--- application/src/Visualization/general.js
+++ application/src/Visualization/general.js
@@ -1,19 +1,37 @@
-import React, { useContext } from 'react';
-import { ProxyContext } from '../Proxy/state-handling';
+import React, { useState, useRef, useContext } from 'react';
 import Framework from '../D3-framework';
 import Demo from '../CRDTs/LWW-Register/demo';
 
 // General visualisation of the states of the CRDTs
 
-const General = () => {
-  const { proxies } = useContext(ProxyContext);
+const General = ({ dimensions, proxies }) => {
+  const {
+    width,
+    height,
+    margin: { top, right, bottom, left },
+  } = dimensions;
+  const svgRef = useRef(null);
 
   return (
     <div className="visualization-element">
-      <h2>Visualization</h2>
-      {Array.from(proxies).map(([id, proxy]) => {
-        return <Framework key={id} proxy={proxy} />;
-      })}
+      {/* <h2>Visualization</h2> */}
+      <svg
+        id="general-visualization"
+        ref={svgRef}
+        width={width}
+        height={height}
+      />
+      <Framework proxies={proxies} dimensions={dimensions} svgRef={svgRef} />
+      {/* {Array.from(proxies).map(([id, proxy]) => {
+        return (
+          <Framework
+            key={id}
+            proxy={proxy}
+            dimensions={dimensions}
+            svgRef={svgRef}
+          />
+        );
+      })} */}
       {/* <Demo /> */}
     </div>
   );
