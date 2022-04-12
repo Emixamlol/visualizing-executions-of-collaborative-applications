b5f9885f6cce1c547c7a0057404066d537d1f962
maxime <maxime.van.uffelen@vub.be>
2022-04-12 16:17:55 +0200

    sets up the state upon crdt-proxy creation

---
+++ application/src/Proxy/create-state.js
@@ -0,0 +1,37 @@
+export const createState = (crdt, params) => {
+  // create the object to be returned
+  const state = {
+    history: [],
+  };
+
+  switch (crdt) {
+    case 'counter': {
+      state.color = 'red';
+      state.content = 0;
+      state.leftWrapper = ''; // defines how the displayed content is wrapped from the left
+      state.rightWrapper = ''; // defines how the displayed content is wrapped from the right
+      break;
+    }
+
+    case 'register': {
+      state.color = 'green';
+      state.content = [0, 0];
+      state.leftWrapper = '('; // defines how the displayed content is wrapped from the left
+      state.rightWrapper = ')'; // defines how the displayed content is wrapped from the right
+      break;
+    }
+
+    case 'set': {
+      state.color = 'blue';
+      state.content = [];
+      state.leftWrapper = '{'; // defines how the displayed content is wrapped from the left
+      state.rightWrapper = '}'; // defines how the displayed content is wrapped from the right
+      break;
+    }
+
+    default:
+      throw new Error('this CRDT does not exist');
+  }
+
+  return state; // return the state object
+};
