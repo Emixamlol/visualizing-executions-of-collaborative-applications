e13b2766be777de3b3b6cc580353dbd9571f9998
maxime <maxime.van.uffelen@vub.be>
2022-04-21 19:24:03 +0200

    implemented d3 circle which will represent the crdt's basic state

--- application/src/D3-framework/basic-state.js
+++ application/src/D3-framework/basic-state.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { ProxyContext } from '../Proxy/state-handling';
+import * as d3 from 'd3';
 
 /**
  *
@@ -7,9 +8,43 @@ import { ProxyContext } from '../Proxy/state-handling';
  */
 
 const BasicState = ({ dimensions, svgRef }) => {
-  const proxies = React.useContext(ProxyContext);
+  const {
+    height,
+    width,
+    margin: { top, right, bottom, left },
+  } = dimensions;
 
-  return <div>Basic State</div>;
+  const { proxies } = React.useContext(ProxyContext);
+
+  const localHeight = (parseInt(height, 10) * visualViewport.height) / 100;
+  const localWidth = (parseInt(width, 10) * visualViewport.width) / 100;
+
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
+    const basicStates = svg.append('g');
+
+    console.log(Array.from(proxies));
+
+    basicStates
+      .selectAll('circle')
+      .data(Array.from(proxies))
+      .enter()
+      .append('circle')
+      .attr('cx', ([id, proxy]) => 170)
+      .attr('cy', ([id, proxy], idx) => idx * (localHeight / 10) + top)
+      .attr('r', 10)
+      .attr('fill', ([id, proxy]) => {
+        console.log(proxy);
+        return proxy.getState().color;
+      });
+
+    return () => {
+      basicStates.remove();
+      // cleanup function
+    };
+  }, [proxies]);
+
+  return null;
 };
 
 export default BasicState;
