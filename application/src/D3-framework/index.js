e836a6229dd2b924bf4da323e5052ef6a01793a2
maxime <maxime.van.uffelen@vub.be>
2022-04-21 23:31:24 +0200

    removed unnecessary comments

--- application/src/D3-framework/index.js
+++ application/src/D3-framework/index.js
@@ -6,15 +6,6 @@ import Text from './text';
 import BasicState from './basic-state';
 
 const Index = ({ dimensions, svgRef }) => {
-  /* const {
-    width,
-    height,
-    margin: { top, right, bottom, left },
-  } = dimensions; */
-
-  // const [state, setState] = useState(proxy.getState());
-  // console.log(state);
-
   const { proxies } = useContext(ProxyContext);
 
   console.log(proxies);
@@ -22,19 +13,7 @@ const Index = ({ dimensions, svgRef }) => {
   // useEffect hook, the function defined within this hook is called each time the component is rendered again
   useEffect(() => {
     const svg = d3.select(svgRef.current);
-    // const idDiv = svg.append('g');
 
-    /* svg
-      .selectAll('text')
-      .data(Array.from(proxies))
-      .enter()
-      .append('text')
-      .attr('x', (d, i) => i * 20)
-      .attr('y', 70)
-      .attr('fill', 'black')
-      .text(([id, proxy]) => id); */
-
-    // console.log(proxy);
     console.log(svg);
 
     return () => {
