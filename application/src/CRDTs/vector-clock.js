6b06d00ac7206efa6e52cb4133f63ef88f7e1261
maxime <maxime.van.uffelen@vub.be>
2022-04-21 17:19:32 +0200

    implemented vector clocks to be used for timestamping the crdt's events

---
+++ application/src/CRDTs/vector-clock.js
@@ -0,0 +1,49 @@
+// vector clock used as timestamp for the crdts
+export default class VectorClock {
+  #vector;
+  constructor(n) {
+    this.length = n;
+    this.#vector = new Array(n).fill(0);
+
+    this.increase = (pid) => {
+      this.#vector[pid]++;
+    };
+
+    this.merge = (vc) => {
+      const rvc = new VectorClock();
+      for (let i = 0; i < n; i++) {
+        rvc.#vector[i] = Math.max(this.#vector[i], vc.#vector[i]);
+      }
+      return rvc;
+    };
+
+    this.isEqual = (vc) => {
+      for (let i = 0; i < n; i++) {
+        if (this.#vector[i] !== vc.#vector[i]) return false;
+      }
+      return true;
+    };
+
+    this.lessOrEqual = (vc) => {
+      for (let i = 0; i < n; i++) {
+        for (let i = 0; i < n; i++) {
+          if (this.#vector[i] > vc.#vector[i]) return false;
+        }
+        return true;
+      }
+    };
+
+    this.less = (vc) => {
+      for (let i = 0; i < n; i++) {
+        if (
+          this.#vector[i] > vc.#vector[i] ||
+          this.#vector[i] === vc.#vector[i]
+        )
+          return false;
+      }
+      return true;
+    };
+
+    this.printClock = () => this.#vector.toString();
+  }
+}
