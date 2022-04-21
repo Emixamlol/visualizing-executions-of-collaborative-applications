d236abae3f7231373d604083f5b588121f79b202
maxime <maxime.van.uffelen@vub.be>
2022-04-21 19:24:34 +0200

    xy position now passed as parameter

--- application/src/D3-framework/text.js
+++ application/src/D3-framework/text.js
@@ -7,12 +7,13 @@ import * as d3 from 'd3';
  * @returns svg text
  */
 
-const Text = ({ dimensions, svgRef }) => {
+const Text = ({ dimensions, svgRef, position }) => {
   const {
     height,
     width,
     margin: { top, right, bottom, left },
   } = dimensions;
+  const { x: xpos, y: ypos } = position;
   const { proxies } = React.useContext(ProxyContext);
 
   // convert the vh values to numbers
@@ -28,7 +29,7 @@ const Text = ({ dimensions, svgRef }) => {
       .data(Array.from(proxies))
       .enter()
       .append('text')
-      .attr('x', 50)
+      .attr('x', xpos)
       .attr('y', ([id, proxy], idx) => idx * (localHeight / 10) + top)
       .attr('fill', ([id, proxy]) => proxy.getState().color)
       .text(([id, proxy], idx) => {
