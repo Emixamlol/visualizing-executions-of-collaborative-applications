2e4c81d1c4794bcbb1a106ad0487d036d237d1d9
maxime <maxime.van.uffelen@vub.be>
2022-04-26 22:30:55 +0200

    modified the name

--- application/src/D3-framework/replicas.js
+++
@@ -1,9 +0,0 @@
-// display the different replicas of a crdt object in an ellipse as in Figure 7 from 'A comprehensive study of Convergent and Commutative Replicated Data Types'
-
-import React from 'react';
-
-const Replicas = () => {
-  return <div>Replicas</div>;
-};
-
-export default Replicas;
