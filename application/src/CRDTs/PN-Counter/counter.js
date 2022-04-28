5785b77c7f4c3e9a97658ca036351858d383a0ec
maxime <maxime.van.uffelen@vub.be>
2022-04-28 18:18:14 +0200

    fixed increment and decrement

--- application/src/CRDTs/PN-Counter/counter.js
+++ application/src/CRDTs/PN-Counter/counter.js
@@ -3,35 +3,24 @@ import VectorClock from '../vector-clock';
 export default class PN_counter {
   #P; // increments
   #N; // decrements
-  #id; // id of the replica
   #pid; // id of the process handling the replica
   #timestamp; // vector clock
 
   constructor(n, pid) {
-    this.#id = 0;
     this.#pid = pid;
     this.#timestamp = new VectorClock(n);
     this.#P = new Array(n).fill(0);
     this.#N = new Array(n).fill(0);
 
-    // help method to get the replica's id
-    this.myID = () => {
-      let res = this.#id;
-      this.#id = (this.#id + 1) % n;
-      return res;
-    };
-
     // update
     this.increment = () => {
-      let g = this.myID();
-      this.#P[g]++;
+      this.#P[pid]++;
       this.#timestamp.increase(pid);
     };
 
     // update
     this.decrement = () => {
-      let g = this.myID();
-      this.#N[g]++;
+      this.#N[pid]++;
       this.#timestamp.increase(pid);
     };
 
@@ -68,17 +57,16 @@ export default class PN_counter {
   }
 
   // gives the payload of this replica
-  payload = () => [this.value(), this.printClock()];
+  payload = () => [this.value(), this.getTimestamp()];
 
-  printClock = () => this.#timestamp.printClock();
+  getTimestamp = () => this.#timestamp.getVector();
 
   specificSate = () =>
     [
       this.value(),
-      this.#id,
       this.#P,
       this.#N,
       this.#pid,
-      this.#timestamp.printClock,
+      this.#timestamp.getVector(),
     ].slice(); // method to get the entire state of a counter
 }
