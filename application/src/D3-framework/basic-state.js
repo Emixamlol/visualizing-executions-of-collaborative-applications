4e05a7de613fa0c2f0e674ed6eb9887edf55c2bd
maxime <maxime.van.uffelen@vub.be>
2022-05-01 23:36:08 +0200

    refactored using custom 'useProxyData' hook

--- application/src/D3-framework/basic-state.js
+++ application/src/D3-framework/basic-state.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
-import { ProxyContext } from '../Proxy/state-handling';
+// import { ProxyContext } from '../Proxy/state-handling';
+import { useProxyData } from '../CustomHooks/useProxyData';
 import * as d3 from 'd3';
 
 /**
@@ -9,45 +10,82 @@ import * as d3 from 'd3';
 
 const BasicState = ({ dimensions, svgRef }) => {
   const {
-    height,
-    width,
     margin: { top, right, bottom, left },
   } = dimensions;
 
-  const { proxies } = React.useContext(ProxyContext);
-  const replicas = (() => {
-    let arr = [];
-    for (const [id, [original, map]] of proxies.entries()) {
-      arr = arr.concat(Array.from(map));
-    }
-    return arr;
-  })();
-
-  const localHeight = (parseInt(height, 10) * visualViewport.height) / 100;
-  const localWidth = (parseInt(width, 10) * visualViewport.width) / 100;
+  const [replicas, proxies] = useProxyData('replicas');
+  const [histories] = useProxyData('histories');
 
   useEffect(() => {
     const svg = d3.select(svgRef.current);
+    const { height, width, x, y } = svgRef.current.getBoundingClientRect();
     const basicStates = svg.append('g');
 
-    console.log(replicas);
-
-    basicStates
+    const circles = basicStates
+      .selectAll('path')
+      .data(histories)
+      .enter()
       .selectAll('circle')
-      .data(replicas)
+      .data((data) => {
+        console.log(data);
+        return data;
+      })
       .enter()
       .append('circle')
-      .attr('cx', ([id, proxy]) => 170)
-      .attr('cy', ([id, proxy], idx) => idx * (localHeight / 10) + top)
+      .attr(
+        'cx',
+        ([id, [msg, payload]], idx) => x + width / 5 + idx * (x + width / 6)
+      )
+      .attr('cy', ([id, [msg, payload]]) => {
+        const idx = replicas.findIndex(
+          ([replicaId, proxy]) => id === replicaId
+        );
+        return idx * (height / 10) + top;
+      })
       .attr('r', 10)
-      .attr('fill', ([id, proxy]) => {
-        console.log(proxy);
+      .attr('fill', ([id, [msg, payload]]) => {
+        const [, proxy] = replicas.find(
+          ([replicaId, proxy]) => id === replicaId
+        );
         return proxy.getState().color;
       });
 
+    const text = basicStates
+      .selectAll('path')
+      .data(histories)
+      .enter()
+      .selectAll('text')
+      .data((data) => data)
+      .enter()
+      .append('text')
+      .attr(
+        'x',
+        ([id, [msg, payload]], idx) => x + width / 5 + idx * (x + width / 6)
+      )
+      .attr('y', ([id, [msg, payload]]) => {
+        const idx = replicas.findIndex(
+          ([replicaId, proxy]) => id === replicaId
+        );
+        return idx * (height / 10) + top - y;
+      })
+      .attr('fill', ([id, [msg, payload]]) => {
+        const [, proxy] = replicas.find(
+          ([replicaId, proxy]) => id === replicaId
+        );
+        return proxy.getState().color;
+      })
+      .text(([id, [msg, payload]]) => `${id}=${JSON.stringify(payload)}`);
+
+    console.log(replicas);
+    console.log(histories);
+
+    // basicStates.selectAll('text').data(histories).enter().append('text').attr('x')
+
     return () => {
-      basicStates.remove();
       // cleanup function
+      circles.remove();
+      text.remove();
+      basicStates.remove();
     };
   }, [proxies]);
 
