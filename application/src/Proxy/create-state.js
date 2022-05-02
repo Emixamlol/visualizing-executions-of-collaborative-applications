a5777fee265e273bbd3e7453f75316f8f52eb5a3
maxime <maxime.van.uffelen@vub.be>
2022-05-02 15:56:04 +0200

    implemented color generation

--- application/src/Proxy/create-state.js
+++ application/src/Proxy/create-state.js
@@ -3,25 +3,23 @@ export const createState = (crdt, params) => {
   const state = {
     history: [], // history of the payload
     payload: [], // current payload
+    color: colorGenerator.next().value, // color of crdt representation
   };
 
   switch (crdt) {
     case 'counter': {
-      state.color = 'DarkGoldenRod';
       state.leftWrapper = ''; // defines how the displayed payload is wrapped from the left
       state.rightWrapper = ''; // defines how the displayed payload is wrapped from the right
       break;
     }
 
     case 'register': {
-      state.color = 'green';
       state.leftWrapper = '('; // defines how the displayed payload is wrapped from the left
       state.rightWrapper = ')'; // defines how the displayed payload is wrapped from the right
       break;
     }
 
     case 'set': {
-      state.color = 'blue';
       state.leftWrapper = '{'; // defines how the displayed payload is wrapped from the left
       state.rightWrapper = '}'; // defines how the displayed payload is wrapped from the right
       break;
@@ -33,3 +31,16 @@ export const createState = (crdt, params) => {
 
   return state; // return the state object
 };
+
+// color generator
+function* ColorGenerator() {
+  const colors = ['DarkMagenta', 'blue', 'brown', 'DarkGoldenRod', 'green'];
+
+  let i = 0;
+  while (true) {
+    yield colors[i];
+    i = i < colors.length - 2 ? i + 1 : 0;
+  }
+}
+
+export const colorGenerator = ColorGenerator();
