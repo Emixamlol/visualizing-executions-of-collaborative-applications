109367b834ee132904ba522a0baf39dc5220a157
maxime <maxime.van.uffelen@vub.be>
2022-05-02 15:58:04 +0200

    implementing dynamic ellipse

--- application/src/D3-framework/replicaEllipse.js
+++ application/src/D3-framework/replicaEllipse.js
@@ -1,55 +1,60 @@
 // display the different replicas of a crdt object in an ellipse as in Figure 7 from 'A comprehensive study of Convergent and Commutative Replicated Data Types'
 
 import React, { useEffect } from 'react';
-import { ProxyContext } from '../Proxy/state-handling';
+// import { ProxyContext } from '../Proxy/state-handling';
+import { useProxyData } from '../CustomHooks/useProxyData';
 import * as d3 from 'd3';
 import ReplicaCircle from './replicaCircle';
+import { arc } from 'd3';
 
 const ReplicaEllipse = ({ dimensions, svgRef }) => {
   const {
-    height,
-    width,
     margin: { top, right, bottom, left },
   } = dimensions;
 
-  const { proxies } = React.useContext(ProxyContext);
+  const [originals, proxies] = useProxyData('originals');
+
   const crdtReplicaIds = (() => {
     let arr = [];
+    // for (const [id, [original, map]] of proxies.entries()) {
+    //   arr = arr.concat([[id].concat(Array.from(map.keys()))]);
+    // }
     for (const [id, [original, map]] of proxies.entries()) {
       arr = arr.concat([[id].concat(Array.from(map.keys()))]);
     }
     return arr;
   })();
 
-  const localHeight = (parseInt(height, 10) * visualViewport.height) / 100;
-  const localWidth = (parseInt(width, 10) * visualViewport.width) / 100;
-
   useEffect(() => {
     const svg = d3.select(svgRef.current);
+    const { height, width, x, y } = svgRef.current.getBoundingClientRect();
     const replicas = svg.append('g');
+
+    console.log(originals);
+    console.log(crdtReplicaIds);
 
     replicas
       .selectAll('text')
-      .data(crdtReplicaIds)
+      .data(originals)
       .enter()
       .append('text')
       .attr('x', (crdt, idx) => {
         if (idx === 0) return 20;
-        const priorAmount = crdtReplicaIds[idx - 1].length;
+        const priorAmount = originals[idx - 1].length;
       })
       .attr('y', (crdt, idx) => {
         if (idx === 0) return 20;
-        const priorAmount = crdtReplicaIds[idx - 1].length;
+        const priorAmount = originals[idx - 1].length;
       })
       .attr('fill', 'black')
       .text((crdt, idx) => {
-        const text = crdtReplicaIds[idx].shift();
+        const text = originals[idx].shift();
         return text;
       });
 
     replicas
       .selectAll('path')
-      .data(crdtReplicaIds)
+      .data(originals)
       .enter()
       .append('path')
       .attr('stroke', 'black')
@@ -57,21 +62,24 @@ const ReplicaEllipse = ({ dimensions, svgRef }) => {
       .attr('stroke-dasharray', '10 10')
       .attr('d', (crdt, idx) => {
         const dynamics = [
-          localHeight,
-          localWidth,
+          height,
+          width,
           top,
           right,
           bottom,
           left,
           crdt.length,
-          crdtReplicaIds.length,
+          originals.length,
+          x,
+          y,
         ];
         const old = 'M 30 200 A 50 200 0 0 1 130 200 A 50 200 0 0 1 30 200';
-        const d = `M 30 ${
-          (localHeight / crdtReplicaIds.length) * (idx + 1)
-        } A 50 ${crdt.length * 10} 0 0 1 130 200 A 50 ${
-          crdt.length * 200
-        } 0 0 1 0 0`;
+        const d = `M ${x} ${y} 
+        A 50 ${crdt.length * 10} 0 0 1 130 ${
+          (height / originals.length) * (idx + 1)
+        } A 50 ${crdt.length * 20} 0 0 1 30 ${
+          (height / originals.length) * (idx + 1)
+        }`;
         return old;
       });
 
@@ -80,15 +88,7 @@ const ReplicaEllipse = ({ dimensions, svgRef }) => {
     };
   }, [proxies]);
 
-  // return null;
-  return (
-    ////<ReplicaCircle
-    ////  dimensions={dimensions}
-    ////  svgRef={svgRef}
-    ////  position={{ x: 30, y: 40 }}
-    /////>
-    null
-  );
+  return <ReplicaCircle dimensions={dimensions} svgRef={svgRef} />;
 };
 
 export default ReplicaEllipse;
