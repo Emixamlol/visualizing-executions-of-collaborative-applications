66e4034ef6cf288e8403b8a1c9e805e153caf06c
maxime <maxime.van.uffelen@vub.be>
2022-07-06 01:09:21 +0200

    deleted crdt type, added it to types.ts instead

--- src/CRDTs/crdt.ts
+++
@@ -1,9 +0,0 @@
-import VectorClock from './vector-clock';
-
-export default interface CRDTInterface<C, R> {
-  merge: (crdt: C) => C;
-
-  payload: () => [R, number[]]; // payload returns array with generic R (result type) and timestamp
-
-  getTimestamp: () => number[];
-}
