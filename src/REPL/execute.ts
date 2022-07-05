be0f11eda15855abfdd64e2cdbfec12938b5f50b
maxime <maxime.van.uffelen@vub.be>
2022-07-06 01:11:04 +0200

    reimplemented part of the REPL

---
+++ src/REPL/execute.ts
@@ -0,0 +1,28 @@
+import { CodeInterface, CRDTMethod } from '../types';
+
+export const execute = ({ proc, parameters }: CodeInterface): void => {
+  switch (proc) {
+    case CRDTMethod.new: {
+      // create a new proxy
+    }
+
+    case CRDTMethod.delete: {
+      // remove a proxy
+    }
+
+    case CRDTMethod.replicate: {
+      // replicate a proxy
+    }
+
+    case CRDTMethod.merge: {
+      // merge two proxies
+    }
+
+    case CRDTMethod.apply: {
+      // apply a proxy method to a proxy
+    }
+
+    default:
+      throw new Error('cannot execute invalid code');
+  }
+};
---
+++ src/REPL/read.ts
@@ -0,0 +1,36 @@
+import { CodeInterface, Keyword } from '../types';
+
+export const read = (code: string): CodeInterface => {
+  const toParse = `toParse: ${code}`;
+  console.log(toParse);
+
+  const parsed = code.split(/\s|\(|\)|,|\[|\]|=/i).filter((el) => el !== ''); // transforms the string of code into an array of tokens
+  const keyword = parsed.shift(); // get the first word of the parsed code
+
+  let [proc, parameters] = ['', ['']];
+
+  switch (keyword) {
+    case Keyword.const:
+      proc = parsed.splice(1, 1)[0];
+      parameters = parsed;
+      break;
+
+    case Keyword.delete:
+    case Keyword.replicate:
+      proc = keyword;
+      parameters = parsed;
+      break;
+
+    default: // in this case the first word was a variable name; the next word is then the keyword
+      const fn = parsed.shift();
+      if (!['query', 'merge', 'compare', 'apply'].includes(fn))
+        throw new Error('cannot read invalid code');
+
+      proc = fn;
+      parameters = [keyword, ...parsed];
+  }
+
+  console.log(`reading: proc = ${proc}, parameters = ${parameters}`);
+
+  return { proc, parameters };
+};
