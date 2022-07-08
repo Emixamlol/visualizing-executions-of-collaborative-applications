b525820478481abbb5d138ea7a71795f63fd5e7d
maxime <maxime.van.uffelen@vub.be>
2022-07-08 14:21:02 +0200

    index file used to import all (already implemented) crdts

---
+++ src/CRDTs/index.ts
@@ -0,0 +1,7 @@
+// import this file to import every CRDT
+
+import TwoPhase_Set from './2P-Set/set';
+import LWW_Register from './LWW-Register/register';
+import PN_Counter from './PN-Counter/counter';
+
+export { TwoPhase_Set, LWW_Register, PN_Counter };
