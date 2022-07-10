43006fa51c32d3e71d1a9da43210ffe740cbf4ca
maxime <maxime.van.uffelen@vub.be>
2022-07-10 23:29:26 +0200

    minor boilerplate setup

--- src/D3-framework/framework.ts
+++ src/D3-framework/framework.ts
@@ -1,5 +1,10 @@
 // main d3 framework file, it defines and exports the main class
 
+import { Strategy } from '../types/d3-framework-types';
+import { StateInterface } from '../types/proxy-types';
+
 export default class Framework {
   private strategy: Strategy; // visualization strategy
+
+  constructor(crdtState: StateInterface) {}
 }
