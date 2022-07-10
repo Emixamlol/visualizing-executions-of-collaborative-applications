9b65ecdde3e974aa815b1edad9d3e5669862ac11
maxime <maxime.van.uffelen@vub.be>
2022-07-10 23:28:35 +0200

    implemented the methods

--- src/REPL/execute.ts
+++ src/REPL/execute.ts
@@ -1,35 +1,55 @@
 import { ParsedCode } from '../types/repl-types';
-import { ProxyMethod } from '../types/proxy-types';
+import { ID, ProxyMethod } from '../types/proxy-types';
+import {
+  addProxy,
+  applyToProxy,
+  mergeProxy,
+  queryProxy,
+  removeProxy,
+  replicateProxy,
+} from '../main';
+import { CRDTtype } from '../types/crdt-types';
 
 export const execute = ({ proc, parameters }: ParsedCode): void => {
   switch (proc) {
     case ProxyMethod.new: {
       // create a new proxy
-      console.log(`Executing ${proc} with parameters ${parameters}`);
+      const [id, crdt, ...params] = parameters;
+      addProxy(id, CRDTtype[crdt], params);
+      break;
+    }
+
+    case ProxyMethod.query: {
+      const [id, ...params] = parameters;
+      queryProxy(id, params);
       break;
     }
 
     case ProxyMethod.delete: {
       // remove a proxy
-      console.log(`Executing ${proc} with parameters ${parameters}`);
+      const [id] = parameters;
+      removeProxy(id);
       break;
     }
 
     case ProxyMethod.replicate: {
       // replicate a proxy
-      console.log(`Executing ${proc} with parameters ${parameters}`);
+      const [idToReplicate, replicaId] = parameters;
+      replicateProxy(idToReplicate, replicaId);
       break;
     }
 
     case ProxyMethod.merge: {
       // merge two proxies
-      console.log(`Executing ${proc} with parameters ${parameters}`);
+      const [id, other] = parameters;
+      mergeProxy(id, other);
       break;
     }
 
     case ProxyMethod.apply: {
       // apply a proxy method to a proxy
-      console.log(`Executing ${proc} with parameters ${parameters}`);
+      const [id, fn, ...params] = parameters;
+      applyToProxy(id, fn, params);
       break;
     }
 
