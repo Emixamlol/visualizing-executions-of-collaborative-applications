1952f33682c75f095b75fdc12b6333ce3220b84c
maxime <maxime.van.uffelen@vub.be>
2022-07-14 13:15:20 +0200

    added Visualization interface to be implemented by visualization strategies

--- src/types/d3-framework-types.ts
+++ src/types/d3-framework-types.ts
@@ -1,5 +1,21 @@
+import { ID, StateInterface } from './proxy-types';
+
 // visualization strategy
 export enum Strategy {
   time = 'time',
   replica = 'replica',
+}
+
+export interface VisualizationInterface {
+  visualize(
+    svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>,
+    replicas: ReplicaInterface[]
+  ): void;
+
+  update(): void;
+}
+
+export interface ReplicaInterface {
+  id: ID;
+  state: StateInterface;
 }
