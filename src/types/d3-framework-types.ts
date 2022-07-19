398c38edbde95a74eabb5b0394269e81c40911fc
maxime <maxime.van.uffelen@vub.be>
2022-07-19 21:01:49 +0200

    implemented Data type for the data which gets handled by the d3 framework; implemented reusable chart pattern

--- src/types/d3-framework-types.ts
+++ src/types/d3-framework-types.ts
@@ -9,7 +9,7 @@ export enum Strategy {
 export interface VisualizationInterface {
   visualize(
     svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>,
-    replicas: ReplicaInterface[]
+    replicas: Array<ReplicaInterface>
   ): void;
 
   update(): void;
@@ -19,3 +19,47 @@ export interface ReplicaInterface {
   id: ID;
   state: StateInterface;
 }
+
+export type Data = Array<[ID, Array<ReplicaInterface>]>; // type of the entire data to be sent to the d3 framework
+
+// different kinds of processed data for different visualization parts
+export type ProcessedData = {
+  objects: Array<ID>;
+  replicas: Array<ReplicaInterface>;
+};
+
+export type margin = {
+  top: number;
+  right: number;
+  bottom: number;
+  left: number;
+};
+
+/** ---------------------------------------------------------------------------------------------------------------------
+ *
+ * Reusable Visualization Components
+ *
+ *
+ * The following interfaces define the API's for reusable visualization components which can be used as building blocks in order to combine different data visualizations according to different needs
+ *
+ */
+
+export interface ReusablePatternInterface {
+  (selection: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>): void;
+
+  width(): number;
+  width(value: number): ReusablePatternInterface;
+
+  height(): number;
+  height(value: number): ReusablePatternInterface;
+
+  margin(): margin;
+  margin(value: margin): ReusablePatternInterface;
+
+  data(): Data;
+  data(value: Data): ReusablePatternInterface;
+}
+
+export interface ReusableObjectEllipse extends ReusablePatternInterface {}
+
+export interface ReusableObjectCircle extends ReusablePatternInterface {}
