0f1a56965e066e548d61d6a5127635dd58302738
maxime <maxime.van.uffelen@vub.be>
2022-07-19 21:06:41 +0200

    invoke the reusable visualizations

--- src/D3-framework/index.ts
+++ src/D3-framework/index.ts
@@ -1,161 +1,89 @@
 // main d3 framework file, it defines and exports the main class
 import * as d3 from 'd3';
+import { payload } from '../types/crdt-types';
 
 import {
-  ReplicaInterface,
+  Data,
   Strategy,
+  ReusablePatternInterface,
   VisualizationInterface,
+  ReplicaInterface,
 } from '../types/d3-framework-types';
-import { ID, StateInterface } from '../types/proxy-types';
+import { ID, Message, StateInterface } from '../types/proxy-types';
+import { visualization } from './Reusable-blocks';
+import { drawObjectCircle } from './Reusable-blocks/object-circle';
+import { drawObjectEllipse } from './Reusable-blocks/object-ellipse';
 import { strategies } from './Strategies';
 
-// data
-const replicas: ReplicaInterface[] = [];
-const merges = [];
-
-let drawer: VisualizationInterface = strategies.get(Strategy.time); // visualizes the data
-
-const color = d3.scaleOrdinal(d3.schemeCategory10);
-
-const svg = d3
-  .select('div.visualization > div.visualization-element')
-  .append('svg')
-  .attr('width', 960)
-  .attr('height', 500);
-
-console.log(svg.node());
-
-// methods which will be invoked by the proxies
-export const sendState = (id: ID, state: StateInterface): void => {
-  replicas.push({ id, state });
-  visualize();
-};
-
-export const update = (): void => {
-  drawer.update();
-};
-
-export const visualize = (): void => {
-  // main method: visualizes all the current data
-  drawer.visualize(svg, replicas);
-};
-
-// ! --------------------------- TO BE DELETED ---------------------------
-
-const width = 1400;
+// constants
+const width = 960;
 const height = 500;
+const margin = { top: 20, right: 20, bottom: 20, left: 20 };
 
-export const svgtobedeleted = d3
-  .select('body')
+const svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any> = d3
+  .select('div.visualization > div.visualization-element')
   .append('svg')
   .attr('width', width)
   .attr('height', height);
 
-const n = 100;
+const vis: ReusablePatternInterface = visualization()
+  .width(width)
+  .height(height);
 
-// svg
-//   .append('g')
-//   .selectAll('rect')
-//   .data(d3.range(n))
-//   .join('rect')
-//   .attr('y', (d) => d * 20)
-//   .attr('width', width)
-//   .attr('height', 10)
-//   .attr('mask', 'url(#mask-1)');
-// .attr('mask', (d) => (d * 20) 'url(#mask-1)');
+const num: number = visualization().width();
 
-svgtobedeleted
-  .append('g')
-  .selectAll('g')
-  .data(d3.range(d3.symbolsFill.length))
-  .join((enter) =>
-    enter
-      .append('g')
-      .selectAll('rect')
-      .data(d3.range(n))
-      .join((enter) =>
-        enter
-          .append('rect')
-          .attr('x', (d) => d * (width / 7))
-          .attr('y', (d) => d * 20)
-          .attr('width', width / 7)
-          .attr('height', 10)
-          .attr('mask', (d) => {
-            const length = width / 7;
-            const x = d * 20;
-            const range = (x - (x % length)) / length;
-            return range % 2 === 0 ? 'url(#mask-1)' : 'url(#mask-2)';
-          })
-      )
-  );
+console.log(svg.node());
 
-const renderMask = (
-  selection: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>,
-  id: string,
-  inverted: boolean
-) => {
-  const mask = selection.append('mask').attr('id', id);
+svg.call(vis);
 
-  mask
-    .append('rect')
-    .attr('width', width)
-    .attr('height', height)
-    .attr('fill', inverted ? 'black' : 'white');
-
-  // mask
-  //   .append('g')
-  //   .attr('transform', `translate(${width / 2}, ${height / 2})`)
-  //   .append('path')
-  //   .attr('d', d3.symbol(d3.symbolsFill[4], 30000)())
-  //   .attr('fill', inverted ? 'white' : 'black');
-
-  // const g = mask
-  //   .append('g')
-  //   .attr('transform', `translate(${width / 2}, ${height / 2})`);
-  const [range, symbols] = [d3.range, d3.symbolsFill];
-
-  mask
-    .selectAll('g')
-    .data(range(symbols.length))
-    .join((enter) => {
-      console.log(enter);
-
-      return enter
-        .append('g')
-        .attr('transform', (d) => `translate(${d * 200 + 100}, ${height / 2})`)
-        .append('path')
-        .attr('d', (d) => d3.symbol(symbols[d], 10000)())
-        .attr('fill', (d) => (d % 2 === 0 ? 'black' : 'white'));
-      // .attr('fill', inverted ? 'white' : 'black')
-    });
+const id: ID = 'x';
+const payload: payload = ['payload', [0]];
+const replica: ReplicaInterface = {
+  id,
+  state: {
+    history: [{ msg: Message.initialized, payload }],
+    payload,
+    merges: [],
+    color: 'blue',
+  },
 };
 
-// renderMask(svg, 'mask-1', true);
-// renderMask(svg, 'mask-2', false);
+const id2: ID = 'y';
+const payload2: payload = ['payload', [0]];
+const replica2: ReplicaInterface = {
+  id: id2,
+  state: {
+    history: [{ msg: Message.initialized, payload }],
+    payload,
+    merges: [],
+    color: 'green',
+  },
+};
 
-svgtobedeleted
-  .call(renderMask, 'mask-1', false)
-  .call(renderMask, 'mask-2', true);
+const data: Data = [
+  [id, [replica, replica, replica, replica, replica]],
+  [id2, [replica2]],
+];
 
-svgtobedeleted
-  .append('g')
-  .selectAll('rect')
-  .data(d3.range(n))
-  .join('rect')
-  .attr('x', (d) => d * 20)
-  .attr('width', 10)
-  .attr('height', height)
-  .attr('mask', 'url(#mask-2)');
-// .attr('mask', (d) => {
-//   const length = width / 7;
-//   const x = d * 20;
-//   const range = (x - (x % length)) / length;
-//   return range % 2 === 0 ? 'url(#mask-1)' : 'url(#mask-2)';
-// });
+const objectEllipse = drawObjectEllipse()
+  .width(width)
+  .height(height)
+  .margin(margin)
+  .data(data);
 
-const rangeWidth = 200;
-const x = 401;
-const range = (x - (x % rangeWidth)) / rangeWidth;
-console.log(x, range);
+const objectCircle = drawObjectCircle()
+  .width(width)
+  .height(height)
+  .margin(margin)
+  .data(data);
 
-// ! --------------------------- TO BE DELETED ---------------------------
+svg.call(objectEllipse).call(objectCircle);
+
+// Framework
+export const main = () => {
+  const data: Data = [];
+
+  let visualizer: VisualizationInterface = strategies.get(Strategy.time); // visualizes the data
+
+  const color = d3.scaleOrdinal(d3.schemeCategory10);
+};
