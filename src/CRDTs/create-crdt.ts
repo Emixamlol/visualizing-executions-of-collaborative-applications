5b19a021e75a61f07b7c335306fa8e6bb9db1407
maxime <maxime.van.uffelen@vub.be>
2022-08-05 17:00:16 +0200

    added function for adding revisited crdts

--- src/CRDTs/create-crdt.ts
+++ src/CRDTs/create-crdt.ts
@@ -1,5 +1,10 @@
-import { CRDTtype, CRDTInterface } from '../types/crdt-types';
+import {
+  CRDTtype,
+  CRDTInterface,
+  StateBasedInterface,
+} from '../types/crdt-types';
 import { PN_Counter, LWW_Register, TwoPhase_Set } from './index';
+import RevisitedCounter from './PN-Counter/revisited-counter';
 
 // method to instantiate a CRDT
 export const createCRDT = (crdt: CRDTtype, params: string[]): CRDTInterface => {
@@ -21,5 +26,29 @@ export const createCRDT = (crdt: CRDTtype, params: string[]): CRDTInterface => {
 
     default:
       throw new Error('this CRDT does not exist');
+  }
+};
+
+// revisited
+export const revisitedCreateCRDT = (
+  type: CRDTtype,
+  params: string[]
+): StateBasedInterface<any, any, any> => {
+  switch (type) {
+    case CRDTtype.counter:
+      const [n, pid] = params.map((value) => parseInt(value, 10));
+      return new RevisitedCounter(n, pid);
+
+    case CRDTtype.register:
+      throw Error('revisited register not implemented');
+
+    case CRDTtype.set:
+      throw Error('revisited set not implemented');
+
+    default:
+      const assertUnreachable = (x: never): never => {
+        throw new Error('CRDT type does not exist');
+      };
+      assertUnreachable(type);
   }
 };
