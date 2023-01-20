bdd485ab3dbe39e66c44ad375a29fc0c67229196
maxime <maxime.van.uffelen@vub.be>
2023-01-20 10:47:29 +0100

    implemented reusable valuePair method

--- src/D3-framework/Reusable-blocks/Patterns/value-pair.ts
+++ src/D3-framework/Reusable-blocks/Patterns/value-pair.ts
@@ -7,11 +7,80 @@ export const valuePair = (): ReusableValuePair => {
   let height: number;
   let margin: margin;
   let replicaId: ID;
+  let tuples: Array<[string, ID]>;
 
   const my: ReusableValuePair = (
     selection: d3.Selection<SVGSVGElement, unknown, HTMLElement, any>
   ) => {
+    // set scales
+    const x = margin.left * 2 + 50;
+
+    const y = margin.top * 2 + 50;
+
+    const t = d3.transition().duration(1000);
+
+    const cx = x + 300;
+
+    const sqrtScale = d3.scaleSqrt().domain([0, 100]).range([0, 50]);
+
+    // process data
+
+    // visualization
     const htmlClass = 'crdt-value-pair';
+
+    const g = selection
+      .selectAll(`g.${htmlClass}`)
+      .data([null])
+      .join('g')
+      .attr('class', htmlClass);
+
+    // visualize elements
+
+    const positionSet = (tspan) => {
+      tspan
+        .attr('class', replicaId)
+        .attr('x', x)
+        .attr('y', (d, i) => y + i * 20)
+        .text((d) => d[0]);
+    };
+
+    g.selectAll('text')
+      .data([null])
+      .join(
+        (enter) =>
+          enter
+            .append('text')
+            .attr('class', replicaId)
+            .attr('x', x)
+            .attr('y', y)
+            .selectAll('tspan')
+            .data(tuples)
+            .join((enter) => enter.append('tspan').call(positionSet)),
+        (update) =>
+          update
+            .selectAll('tspan')
+            .data(tuples)
+            .join(
+              (enter) => enter.append('tspan').call(positionSet),
+              (update) => update.call(positionSet)
+            )
+      );
+
+    // visualize unique identifiers associated to the elements
+
+    const positionCircle = (circle) => {
+      circle
+        .attr('r', (d) => sqrtScale(d[1]))
+        .attr('cx', cx)
+        .attr('cy', (d, i) => y + i * 20);
+    };
+
+    g.selectAll('circle')
+      .data(tuples)
+      .join(
+        (enter) => enter.append('circle').call(positionCircle),
+        (update) => update.call(positionCircle)
+      );
   };
 
   my.width = function (_?: number): any {
@@ -28,6 +97,10 @@ export const valuePair = (): ReusableValuePair => {
 
   my.replicaId = function (_?: ID): any {
     return arguments.length ? ((replicaId = _), my) : replicaId;
+  };
+
+  my.tuples = function (_?: Array<[string, ID]>): any {
+    return arguments.length ? ((tuples = _), my) : tuples;
   };
 
   return my;
