b0698d3f524b0db31a71871652fde3807e5d0dad
maxime <maxime.van.uffelen@vub.be>
2023-02-10 13:52:53 +0100

    refactored update to include general-svg and specific-svg update methods

--- src/D3-framework/index.ts
+++ src/D3-framework/index.ts
@@ -1,11 +1,18 @@
 // main d3 framework file, it defines the api for the library
-import { update } from './Svg/main-svg';
+import { Data } from '../types/d3-framework-types';
+import { update as mainSvgUpdate } from './Svg/main-svg';
 import {
+  update as specificSvgUpdate,
   drawFlag,
   drawCounter,
   drawRegister,
   drawSet,
   drawTombstone,
 } from './Svg/specific-svg';
+
+const update = (data: Data) => {
+  mainSvgUpdate(data);
+  specificSvgUpdate(data);
+};
 
 export { update, drawCounter, drawFlag, drawRegister, drawSet, drawTombstone };
